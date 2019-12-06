485e0cd87acc14804c1bd4654de3e047a21e7eb3
Yihnew Eshetu <yte9pc@virginia.edu>
2019-12-06 01:22:10 -0500

    Final Copy

--- FIFAApp.py
+++ FIFAApp.py
@@ -3,11 +3,16 @@
 
 # ## University of Virginia - DSI CS5010 Final Project
 # ### Fall 2019
-# > HuiLin Chang, Yihnew Eshetu, Binyong Liang, and Travis Vitello
+# > Yihnew Eshetu (yte9pc),
+# Travis Vitello (tjv9qh),
+# Binyong Liang (bl9m),
+# Gladies Huilin (hc5hq)
+# <br> https://github.com/yte9pc/CS5010-Final-Project/tree/master
+# 
 
 # ### Import neccessary packages
 
-# In[17]:
+# In[34]:
 
 
 from bokeh.models.annotations import Title
@@ -48,7 +53,7 @@ import seaborn as sns
 # We have used pie charts, box plots, bar plots to graphically display our findings. For example, we learned that some countries have abundant good soccer players, and some countries have limited amount of players. We learned that certain physical attributes are more important for certain type of players. A good example is that goalies are usually tall players. Very importantly, we implemented graphical user interaction, and users can extract their desired information easily by a few mouse clicks. By the way, one of user story was which country has the best team, i.e. in our attempt to beat Paul the Octopus. The best team, based on the "Overall rating", is Spain, and the second best team is France. Considering France won the world cup last year, and Spain won it in 2010, we might have a good chance against Paul! 
 # 
 
-# In[18]:
+# In[35]:
 
 
 # Read in csv file
@@ -58,7 +63,7 @@ fifa = fifa.drop(columns='Unnamed: 0')
 
 # In 2019, a total of 159 countries were represented in FIFA 20. The top three participating countries based on the numbers of players are England, Germany and Spain.
 
-# In[19]:
+# In[36]:
 
 
 numplayers = fifa['Country'].value_counts().sort_values(ascending = False).head(10)
@@ -84,11 +89,11 @@ barlabel(numplayers)
 
 # Below is the distribution of top 10 participating country. Brazil has the highest mean overall rating, while Japan has the lowest mean overall rating. However, Germany has the largest distrubtion as you can see many outliers.
 
-# In[20]:
+# In[37]:
 
 
 # Top contries overall rating distribution
-some_countries = ('England', 'Germany', 'Spain', 'France', 'Argentina', 'Italy', 'Columbia', 'Japan', 'Brazil', 'Holland')
+some_countries = ('England', 'Germany', 'Spain', 'France', 'Argentina', 'Italy', 'Colombia', 'Japan', 'Brazil', 'Holland')
 df_countries = fifa.loc[fifa['Country'].isin(some_countries) & fifa['Overall Rating']].sort_values(by = 'Country', ascending = True)
 
 plt.rcParams['figure.figsize'] = (12, 7)
@@ -100,7 +105,7 @@ ax.set_title(label = 'Distribution of Overall Rating of Players from Top 10 Coun
 
 # The bar graph below reveals the average overall rating of the top 10 countries. Brazil having the highest average overall rating does not necessarily mean the have the best players.
 
-# In[21]:
+# In[38]:
 
 
 by_country = fifa[fifa.Country.isin(some_countries)==True]
@@ -121,7 +126,7 @@ barlabel(by_country)
 # <br>
 # User can enter or select 2 players to compare. The pie chart allows a side by side comparsion of each skill Rating between 2 players. The default players selected are the top 2 players, Lionel Messi and Ronaldo.
 
-# In[22]:
+# In[39]:
 
 
 data = fifa[['Name','Pace', 'Shooting','Passing', 'Dribbling', 'Defending', 'Physicality']]
@@ -139,7 +144,7 @@ angles2.sort()
 angles = [ math.radians(i) for i in angles2]
 
 
-# In[23]:
+# In[40]:
 
 
 name = fifa.Name.tolist()[0]
@@ -167,7 +172,7 @@ ax1.grid(True)
 # <br>
 # There are a total of 17 positions in FIFA 20. The most common positions are Center Back (CB), Striker (ST), and Central Midfielder (CM)
 
-# In[24]:
+# In[41]:
 
 
 fig = plt.figure(figsize = (12, 8))
@@ -190,7 +195,7 @@ def barlabelpandas(pandas):
 barlabelpandas(count_positions['Overall Rating'])
 
 
-# In[25]:
+# In[42]:
 
 
 fig = plt.figure(figsize = (12, 8))
@@ -208,13 +213,13 @@ barlabelpandas(count_positions_groups['Overall Rating'])
 
 # Below is the distribution of overall rating per positions for each continent. As the graph illustrates, South American has the highest rated players compared to other continents. While, Oceania and Asia have the lowest overall rating.
 
-# In[26]:
+# In[43]:
 
 
 ax = sns.boxplot(x="Position Group", y="Overall Rating", hue="Continent", data=fifa, palette="Set2")
 
 
-# In[27]:
+# In[44]:
 
 
 def bestplayers(df, formation, skill):
@@ -272,9 +277,9 @@ def bestformation(df, formation, skill):
     return best
 
 
-# Below are the top 5 national teams in the world. The best teams were determined by calculating the highest overall rating for a country regardless of the formation. As you can see Spain has the best team using the formation 4-5-1, while France, the second best team, uses a 4-3-3 formation.
+# Below are the top 5 national teams in the world. The best teams were determined by calculating the highest overall rating for a country regardless of the formation. As you can see Spain has the best team using the formation 4-5-1, while France, the second best team, using a 4-3-3 formation.
 
-# In[29]:
+# In[45]:
 
 
 def top5countries():
@@ -299,7 +304,7 @@ top5countries()
 # <br>
 # Users need to be able to identify top skill set for each position. Having such information aids in the selection of players when creating a team or selecting what team to use. 
 
-# In[13]:
+# In[46]:
 
 
 # defining the features of players
@@ -311,11 +316,23 @@ for i, val in fifa.groupby(fifa['Position'])[player_features].mean().iterrows():
     print('Position {}: {}, {}, {}, {}'.format(i, *tuple(val.nlargest(4).index)))
 
 
+# ## Beyond the Original Specifications 
+# As soon as we formed the group, we have unanimously agreed that we will use the web-scrapping techniques to fetch our data. In the end, we were able to scrap more than 16,000 entries of current soccer player stats from the www.futbin.com website. In addition to web scrapping, we have cleaned up and augmented the data for our later applications. For example, we have to delete a few cases of entry redundancy, as certain players appeared more than once within our dataset. We were able to handle this issue by removing older versions of players and keeping the latest version of the player. We also implemented a continent map function using the pycountry package. This package allowed us to map each country to a continent using the alpha 2 code. To also defined each position into 4 major "Position Groups" for playing positions. Some of these changes are necessitated by our specific user stories, for example, we wanted to create a best continental team or selecting best team based on formation.
+# Some of changes are due to the fact that FIFA associations are not in 1 to 1 correlation with countries. In the end, we were albe to clean up and augment our web scrapped data for our later application. For details of our web scrapping, please refer to our python file: FIFADataWebscrapper.py  
+# 
+# The second important implementation was our graphical user interface. We first started this project aiming to learn information such as correlations between players' physical attributes and skill sets, or between playing positions and skill sets. We quickly realized that each one of us has different interests. The best way is for users to decide the types of information they want to learn. We learned that a Bokeh server can employed to achieve our goal. The initial implementation of Bokeh was carried out in MacBook, and we had the program successfully demoed live during our last class session. However, during additional testing, we later found out that some code cannot be run on windows. Our team was able to solve the issue and now our program can be run in both platforms, and users have total control about the types of correlations, starting 11 players, etc.    
+# 
+# We believe that we have used a number of unique queries and graphs for extracting and displaying our data. For example, we used fuzzy matching to manipulate the country code of the data entry, we use a pie chart to compare multiple skill sets between two players simultaneously. Additionally, our best starting 11 algorithm is able to display the best starting eleven based on formation, country, league, skill, or club. All in real time as the users updates their selction. This is the same for our scatter plot and histogram. In addition, all of our interactive graphs include a hoover tool tip that allows the users to hoover over a data point, player, or histogram bar and see more additionally display. Our aim with all of these extra tools was to provide the users with user friendly interactive tool that would respond and answer all of their questions.
+# 
+# The team also used github to keep track of all our python related files and unittests.
+# https://github.com/yte9pc/CS5010-Final-Project/tree/master
+# 
+
 # **Create Slider, MultiSelect, and Input for Scatter Plot**
 # <br>
 # Users have the ability to filter data to a certain player or range of overall rating or skill level. Or select multiple clubs, leagues, countries, or positions. Using this scatter plot users can idenitfy relationships between attributes or group of players.
 
-# In[14]:
+# In[47]:
 
 
 player = AutocompleteInput(title="Player Name", completions=fifa.Name.tolist())
@@ -331,7 +348,7 @@ position = MultiSelect(title="Positions", options=['CF', 'ST', 'RW', 'RF', 'LW',
 # <br>
 # Using columndata source to allow user to hover over datapoint to obtain additional information about each player.
 
-# In[15]:
+# In[48]:
 
 
 axis_map = dict(zip(fifa.select_dtypes(include='int64').columns.values, fifa.select_dtypes(include='int64').columns.values))
@@ -395,7 +412,7 @@ sizing_mode="stretch_width")
 
 # ### Select function updates the plot based on user selection
 
-# In[16]:
+# In[49]:
 
 
 def select_fifa():
@@ -442,7 +459,7 @@ def select_fifa():
 
 # ### Update function calls select_fifa() function to update plot automatically
 
-# In[17]:
+# In[50]:
 
 
 def update():
@@ -500,7 +517,7 @@ l = layout([
 
 # ###  Create Select for Histogram
 
-# In[18]:
+# In[51]:
 
 
 # X axis that user can select from
@@ -515,7 +532,7 @@ skills3 = Select(title="Skills", value = 'Passing', options=hist_x_axis)
 
 # ### Create Histogram and html code for describing the interface
 
-# In[19]:
+# In[52]:
 
 
 arr_src = ColumnDataSource(data=dict(count=[],left=[], right=[], f_count=[], f_interval=[]))
@@ -596,7 +613,7 @@ sizing_mode="stretch_width")
 
 # ### Select function updates the histogram based on user selection
 
-# In[20]:
+# In[53]:
 
 
 def select_hist():
@@ -624,7 +641,7 @@ def select_hist():
 
 # ### Update function calls select_hist() function to update histogram automatically
 
-# In[21]:
+# In[54]:
 
 
 def update3():
@@ -668,7 +685,7 @@ l3 = column(row(desc3),row(club3, league3, country3, position3,skills3),row(p3))
 
 # ### Best Starting 11
 
-# In[22]:
+# In[55]:
 
 
 # Formation to select from
@@ -684,7 +701,7 @@ best_x_axis = ['Pace', 'Shooting', 'Passing', 'Dribbling', 'Defending', 'Overall
 
 # ### Create Select and MultiSelect
 
-# In[23]:
+# In[56]:
 
 
 skills2 = Select(title="Skills", value = 'Overall Rating', options=best_x_axis)
@@ -697,7 +714,7 @@ continent = MultiSelect(title="Continent", options=fifa.sort_values('Continent')
 
 # ### Create Plot of Best Starting 11 and html code for describing the interface
 
-# In[24]:
+# In[57]:
 
 
 source2 = ColumnDataSource(data=dict(x=[], y=[], color=[], legend=[], player=[], club=[], league=[], 
@@ -764,7 +781,7 @@ sizing_mode="stretch_width")
 
 # ### Algorithm for selecting players based on formation and skill
 
-# In[25]:
+# In[58]:
 
 
 def bestplayers(df, formation, skill):
@@ -789,7 +806,7 @@ def bestplayers(df, formation, skill):
 
 # ### Algorithm for selecting any formation and graphing location of players on the field
 
-# In[26]:
+# In[59]:
 
 
 def bestformation(df, formation, skill):
@@ -830,7 +847,7 @@ def bestformation(df, formation, skill):
 
 # ### Select function updates the best starting 11 based on user selection
 
-# In[27]:
+# In[60]:
 
 
 def select_fifa2():
@@ -871,7 +888,7 @@ def select_fifa2():
 
 # ### Update function for best starting 11 when user changes selection
 
-# In[28]:
+# In[61]:
 
 
 def update2():
@@ -918,7 +935,7 @@ inputs2.sizing_mode = "fixed"
 l2 = column(row(desc2),row(inputs2, p2, column(formation, skills2)))
 
 
-# In[29]:
+# In[62]:
 
 
 # Create a panel for each tab
@@ -935,27 +952,25 @@ curdoc().add_root(tabs)
 curdoc().title = "FIFA 2020 Players"
 
 
-# ### Beyond the Original Specifications 
-# As soon as we formed the group, we have unanimously agreed that we will use the web-scrapping techniques to fetch our data. In the end, we were able to scrap more than 16,000 entries of current soccer player stats from the www.futbin.com website. In addition to web scrapping, we have cleaned up and augmented the data for our later applications. For example, we have to delete a few cases of entry redundancy, as certain players appeared more than once within our dataset. We were able to handle this issue by removing older versions of players and keeping the latest version of the player. We also implemented a continent map function using the pycountry package. This package allowed us to map each country to a continent using the alpha 2 code. To also defined each position into 4 major "Position Groups" for playing positions. Some of these changes are necessitated by our specific user stories, for example, we wanted to create a best continental team or selecting best team based on formation.
-# Some of changes are due to the fact that FIFA associations are not in 1 to 1 correlation with countries. In the end, we were albe to clean up and augment our web scrapped data for our later application. For details of our web scrapping, please refer to our python file: FIFADataWebscrapper.py  
-# 
-# The second important implementation was our graphical user interface. We first started this project aiming to learn information such as correlations between players' physical attributes and skill sets, or between playing positions and skill sets. We quickly realized that each one of us has different interests. The best way is for users to decide the types of information they want to learn. We learned that a Bokeh server can employed to achieve our goal. The initial implementation of Bokeh was carried out in MacBook, and we had the program successfully demoed live during our last class session. However, during additional testing, we later found out that some code cannot be run on windows. Our team was able to solve the issue and now our program can be run in both platforms, and users have total control about the types of correlations, starting 11 players, etc.    
-# 
-# We believe that we have used a number of unique queries and graphs for extracting and displaying our data. For example, we used fuzzy matching to manipulate the country code of the data entry, we use a pie chart to compare multiple skill sets between two players simultaneously. Additionally, our best starting 11 algorithm is able to display the best starting eleven based on formation, country, league, skill, or club. All in real time as the users updates their selction. This is the same for our scatter plot and histogram. In addition, all of our interactive graphs include a hoover tool tip that allows the users to hoover over a data point, player, or histogram bar and see more additionally display. Our aim with all of these extra tools was to provide the users with user friendly interactive tool that would respond and answer all of their questions.
-# 
-# ### Testing
+# ## Testing
 # We started testing with unittest as we developed our functions. In total we have 21 unittests for our all of  our functions. With each function being tested at least once, while may being tested multiple tests. For example, for our web scrapping part, we tested that we can reach the website, that the data was scrapped in the correct format and the csv was written. We also have unittests for our support functions such as our country to continent convert and positional group function. We also insured to test all of user select functions, and functions that determined the best starting eleven. In all, all of unittest returned as success for our final program.
 # 
 # Additionally, we tested the portability of our program. As we have alluded to earlier, our program was first implemented heavily on a MacBook. After some struggles, we fulfilled at least one non-functional requirement -- portability. Now, our program runs on both Mac and Windows. 
 # 
-# ### Conclusion
-# 
+# ## Conclusion
 # We were able to successfully web scrap data from the www.futbin.com website. We cleaned and augmented the data, extracted information, and finally turned these data into knowledge for all of our users. A video gamer can use our program to customize his or her soccer team based on different criteria. A FIFA World Cup soccer fan can simulate which national team will win the next World Cup. An EA developer can understand the variation of players skill stats using our scatter plot. We, as part of this team, cherished the opportunity in work in scrum environment. Together we were able to create a project that we are all proud of. 
 # 
-# ### Improvements
+# ## Improvements
+# Some improvements we could make to our project is including historical data of FIFA players for past versions of FIFA. This would allow us to analysis players' skills trends overtime. We could also incorporate a predictive ML model using neural network to predict future player ratings. This would be useful for users such as video gamers and developers. As this would allow them to see a prediction of players in the next years FIFA video game. Futuremore, allowing users to build a team by dragging players from a bin and provding analysis such as chemistry and team strength would be helpful knowledge. Finally, allowing a drill down from the best starting 11 to individual player would be a helpful feature.
 # 
-# ### References
+# ## References
 # 1. https://www.futbin.com/
 # 2. https://www.crummy.com/software/BeautifulSoup/bs4/doc/
 # 3. http://docs.bokeh.org/en/1.3.2/docs/user_guide/server.html
 # 
+
+# In[ ]:
+
+
+
+
